3396f1916080b8524f65ff863029d3ea53e9ab37
LiudmylaKozytska <ludaanishchenko0512@gmail.com>
2023-06-01 00:29:35 +0200

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 
 const Home = lazy(() => import("./pages/Home"));
@@ -8,12 +8,14 @@ const Users = lazy(() => import("./pages/Users"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/users" element={<Users />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
